e6f1510e8df8fc1b18c4c1c30d024c62ad726cde
陈齐翔 <892670992@qq.com>
2017-08-03 10:10:07 +0800

    refs #5, Cover a chche file and Django's unit test file

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from backend.api.LiveRoomAPI import selectRoom
 
 # Create your tests here.
+class LiveRoomAPITestCase(TestCase):
+    def setUp(self):
+        pass
+    def test_selectRoom(self):
+        print("---------begin----------\t\n")
+        print(selectRoom().values_list)
+        print("---------end----------\t\n")
+        print("---------begin----------\t\n")
+        print(selectRoom(id = 1).values_list)
+        print("---------end----------\t\n")
+    
+    def tearDown(self):
+        pass
