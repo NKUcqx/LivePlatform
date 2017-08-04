c933006019ad89bf076ec32b3508ef0ebfa71eef
xuqianwen <1067261983@qq.com>
2017-08-04 02:13:54 +0000

    Add homepage, refs #9

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,24 +4,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as actions from './actions'
-import * as getters from './getters'
+//import * as getters from './getters'
 import * as states from './states'
-import * as mutations from './mutations'
+//import * as mutations from './mutations'
 import tests from './modules/testmodules'
 
 Vue.use(Vuex)
 
 //I don't know what this can do,maybe use for debug I guess
 //this come from document from offcial web of vue
-const debug = process.env.NODE_ENV !== 'production'
+import VueLocalStorage from 'vue-localstorage'
+Vue.use(VueLocalStorage)
 
+const debug = process.env.NODE_ENV !== 'production'
+const STORAGE_KEY = 'vedios-vuejs'
+const state = {
+    vedios: JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+}
+const getters = {
+    vedios:  state => state.vedios
+} 
+const mutations = {
+    addVedio (state, room , teacher, viewers) {
+        state.vedios.push({
+            room,
+            teacher,
+            viewers
+        })
+    },
+    deleteVedio (state, index) {
+        state.vedios.splice(index, 1)
+    }
+}
+const localStoragePlugin = store => {
+  store.subscribe((mutation, { vedios }) => {
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(vedios))
+  })
+}
 export default new Vuex.Store({
-  states,
+  state,
   actions,
   getters,
   mutations,
   modules: {
     tests,
   },
-  strict: debug
+  strict: debug,
+  plugins: [localStoragePlugin]
 })
