3e46fbc2802421cd6f61a55ab3f75e54ad8c0386
陈齐翔 <892670992@qq.com>
2017-08-07 18:08:15 +0800

    Fix a bug in LiveRoom receiver, refs #45

--- backend/models.py
+++ backend/models.py
@@ -121,8 +121,9 @@ def checkEndAndLiving(sender, instance, **kwargs):
     print(instance.end_time)
     if(instance.is_living == True and instance.end_time is not  None):
         raise TypeError("Living Room can't have property end_time")
-    if(instance.is_living == False and instance.end_time is None):
-        raise TypeError("End Room must both have is_living = False and end_time property")
+    if(instance.is_living == False):
+        instance.end_time = timezone.now()
+
 
 class Punishment(models.Model):
     room = models.ForeignKey(LiveRoom, default = get_Room, on_delete = models.CASCADE)
