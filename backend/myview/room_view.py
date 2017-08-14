f76f33a5ec239d68ee0dda738b9b46b8e246e7db
陈齐翔 <892670992@qq.com>
2017-08-14 14:56:36 +0800

    Can convert and unzip now, refs #109

--- backend/myview/room_view.py
+++ backend/myview/room_view.py
@@ -6,14 +6,21 @@ from django.utils import timezone
 from django.views.decorators.http import require_POST, require_GET
 from . import toolkits
 import os
-import hashlib
 import random
+import hashlib
+import zipfile
+import cloudconvert
 
 CODE = toolkits.CODE  # assign to local name to reduce the usage of its long prefix
 bi2obj = toolkits.bi2obj
 # change ImageFieldFile or some other field type into str for JsonResponse
 model_to_json = toolkits.model_to_json
-
+API_key = [ # to get more free conversion chances ..
+            '1ExrXf1WpChC9gvOxnzuR5_unapQx1RqigB4Kmyc-teB3tp8kSybrSU-nrcELHwJWYxljLt8euwJwtgFTrdFsQ',
+            'LRxCwzT2-CLqFqr936dWjknyulqsjFtXVL1GQLcmJhsgG_Ad7503sH98cGcSvFhQe5pRUWmuR4tb448MvgarEg'     
+        ]
+IMG_LIST = ('.jpg', '.jpeg' '.png', '.svg', '.gif')
+SLIDE_LIST = ('.ppt', '.pptx', '.pps', '.pdf')
 #model_to_dict = toolkits.model_to_dict
 
 
@@ -27,27 +34,55 @@ def generate_room_path():
 
 
 def create_folder(file_name):
-    if (os.path.exists(os.path.join('frontend/static/rooms/', file_name))):
+    if (os.path.exists(file_name)):
         return False
     else:
-        os.makedirs(os.path.join('frontend/static/rooms/', file_name))
-        os.mknod(os.path.join('frontend/static/rooms/', file_name, 'log.txt'))
+        os.makedirs(file_name)
+        os.mknod(os.path.join(file_name, 'log.txt'))
         return True
 
 
 def upload_thumbnail(room_id, thumbnail):
-    room = LiveRoom.objects.get(pk=room_id)
+    room = LiveRoom.objects.get(pk = room_id)
     room.thumbnail_path = thumbnail
     room.save()
     return room
 
 
 def upload_slide(room_id, slide):
-    room = LiveRoom.objects.get(pk=room_id)
-    room.slide_path = slide
+    room = LiveRoom.objects.get(pk = room_id)
+    converted_file = convert_file(slide, room.file_name) # upload the slide and download the conversion to room.filename
+    unziped_file = un_zip(converted_file)
+    room.slide_path = unziped_file
     room.save()
     return room
 
+def convert_file(file, upload_to):
+    # TODO: if(type(file) )
+    api = cloudconvert.Api(API_key[1]) # TODO: random.randint(0,1)
+    split_arr = os.path.splitext(file.name)
+    realname = split_arr[0].split('/')[-1]
+    postfix = split_arr[1][1:]
+    process = api.convert({
+        'inputformat': postfix, # get ext then emit '.'
+        'outputformat': 'jpg', # TODO: will change to any type laterly 
+        'input': 'upload',
+        'filename': realname + postfix,
+        'file': file
+    })
+    process.wait()
+    process.download(upload_to)
+    return upload_to + realname + '.zip' # TODO: So ugly..
+
+def un_zip(file_name):
+    zip_file = zipfile.ZipFile(file_name)
+    # file with postfix i.e. 'frontend/static/rooms/ertfhg3456yhj/slide (without .ppt)'
+    split_str = os.path.splitext(file_name)[0]
+    if not os.path.isdir(split_str):
+        os.mkdir(split_str)
+    zip_file.extractall(split_str)  
+    zip_file.close()
+    return split_str
 
 @login_required
 @require_GET
@@ -97,29 +132,44 @@ def getRoomAmount(request):
 @require_POST
 def uploadThumbnail(request):
     # must satisfy: login and is a teacher and has created a live room
-    if (request.user.role == 'T' and 'room' in request.session):
-        room = upload_thumbnail(
-            request.session.get('room').id, request.FILES.get('thumbnail'))
-        return JsonResponse({'room': model_to_json(room)})
-    # because each person can not create other rooms while living
-    elif ('room' not in request. session):
-        return HttpResponse(content=CODE['24'], status=400)
+    if ('room' in request.session):
+        room = request.session['room']
+        if(str(request.user.id) == room['creater']):
+            thumbnail = request.FILES.get('avatar')
+            thumbnail_type = os.path.splitext(thumbnail.name)[1]
+            if (thumbnail_type in IMG_LIST):
+                room = upload_thumbnail(room['id'], thumbnail)
+                return JsonResponse({'room': model_to_json(room)})
+            else:
+                return HttpResponse(content = CODE['20'], status = 415)
+        else:
+            return HttpResponse(content = CODE['12'], status = 401)
     else:
-        return HttpResponse(content=CODE['12'], status=401)
+        return HttpResponse(content = CODE['24'], status = 400)
 
 
 @login_required
 @require_POST
 def uploadSlide(request):
-    if (request.user.role == 'T' and 'room' in request.session):
-        room = upload_slide(
-            request.session.get('room').id, request.FILES.get('slide'))
-        return JsonResponse({'room': model_to_json(room)})
+    if ('room' in request.session):
+        room = request.session['room']
+        if(str(request.user.id) == room['creater']):
+            slide = request.FILES.get('avatar', None)
+            if(slide is not None):
+                slide_type = os.path.splitext(slide.name)[1]
+                # must add dot ! otherwise user could upload file like
+                # "somepdf" instead of "some.pdf"  #any type else ?
+                if (slide_type in SLIDE_LIST):
+                    room = upload_slide(room['id'], slide)
+                    return JsonResponse({'room': model_to_json(room)})
+                else:
+                    return HttpResponse(content = CODE['20'], status=415)
+            return HttpResponse(content = CODE['5']) # no changes, but it is nobody's fault, return 200
+        else:
+            return HttpResponse(content=CODE['12'], status=401)
     # because each person can not create other rooms while living
-    elif ('room' not in request. session):
-        return HttpResponse(content=CODE['24'], status=400)
     else:
-        return HttpResponse(content=CODE['12'], status=401)
+        return HttpResponse(content=CODE['24'], status=400)
 
 
 # TODO create error_log.txt
@@ -132,12 +182,9 @@ def createRoom(request):
     if (request.user.role == 'T' and 'room' not in request.session):
         creater_id = request.user.id
         thumbnail = request.FILES.get('thumbnail', None)
-        slide = request.FILES.get('slide', None)
         name = request.POST.get('name')
-        is_silence = True if request.POST.get('is_silence',
-                                              None) is not None else False
-        is_living = False if request.POST.get('is_living',
-                                              None) is not None else True
+        is_silence = True if request.POST.get('is_silence', None) is not None else False
+        is_living = False if request.POST.get('is_living', None) is not None else True
         file_name = generate_room_path()
         if (create_folder(file_name)):
             room = LiveRoom(
@@ -146,35 +193,19 @@ def createRoom(request):
                 file_name=file_name,
                 is_silence=is_silence,
                 is_living=is_living)
-            if (thumbnail):
-                thumbnail_type = os.path.splitext(thumbnail.name)[1]
-                if (thumbnail_type.endswith(('.jpg', '.png', '.jpeg',
-                                             '.gif'))):
-                    room = upload_thumbnail(room, thumbnail)
-                else:
-                    return HttpResponse(content=CODE['20'], status=415)
-            if (slide):
-                slide_type = os.path.splitext(slide.name)[1]
-                # must add dot ! otherwise user could upload file like
-                # "somepdf" instead of "some.pdf"  #any type else ?
-                if (slide_type.endswith(('.ppt', '.pptx', '.pps', '.swf',
-                                         '.pdf', '.key'))):
-                    room = upload_slide(room, slide)
-                else:
-                    return HttpResponse(content=CODE['20'], status=415)
             room.save()
             room = model_to_json(room)
             room['creater_nickname'] = User.objects.get(
-                pk=room['creater']).nickname
+                pk = room['creater']).nickname
             request.session['room'] = room
             return JsonResponse({'room': room})  # return the new room's id
         else:
-            return HttpResponse(content=CODE['6'], status=500)
+            return HttpResponse(content = CODE['6'], status = 500)
     elif ('room' in request.
           session):  # because each person can not create other rooms while living
-        return HttpResponse(content=CODE['21'], status=400)
+        return HttpResponse(content = CODE['21'], status = 400)
     else:
-        return HttpResponse(content=CODE['12'], status=401)
+        return HttpResponse(content = CODE['12'], status = 401)
 
 
 @login_required
