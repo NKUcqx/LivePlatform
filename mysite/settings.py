9c0d9e301bb678459398aea80933ac1b6415c251
陈齐翔 <892670992@qq.com>
2017-08-15 01:21:02 +0800

    Forget to change settings.py, refs #97

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'group3',
-        'USER': 'jason',
-        'PASSWORD': '',
+        'NAME': 'liveplat',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
