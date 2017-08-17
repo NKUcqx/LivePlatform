e024a977bbafd26e385e5a3c4a9126c1303ef6a1
xuqianwen <1067261983@qq.com>
2017-08-17 14:13:00 +0000

    Change the document for const.js, refs #144

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,10 +1,45 @@
+/**
+ * Module CONST
+ *
+ * @module CONST
+ * @class CONST
+ * @constructor
+ */
 export const CONST = {
+    /**
+     *@property success
+     *@type function
+     */
+    /**
+     *代表成功
+     *@method success
+     *@param {string} str 字符串
+     *@return {string} 返回str + ' Success'
+     */
     success: (str) => {
         return str + ' Success'
     },
+    /**
+     *@property failure
+     *@type function
+     */
+    /**
+     *代表失败
+     *@method failure
+     *@param {string} str 字符串
+     *@return {string} 返回str + ' Failure'
+     */
     failure: (str) => {
         return str + ' Failure'
     },
+    /**
+     *@property login
+     *@type string 'Please Login First'
+     */
     login: 'Please Login First',
+    /**
+     *@property permission
+     *@type string 'Permission Denied'
+     */
     permission: 'Permission Denied'
 }
