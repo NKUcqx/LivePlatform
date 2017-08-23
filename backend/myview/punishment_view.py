58ab40ccbc158b74935d2b78ca305bf08c12e03d
陈齐翔 <892670992@qq.com>
2017-08-23 17:36:52 +0800

    Add check interface firstly, refs #182

--- backend/myview/punishment_view.py
+++ backend/myview/punishment_view.py
@@ -12,17 +12,14 @@ model_to_json = toolkits.model_to_json
 
 def echo(request):
     print('--echo--')
-    get_userid = request.GET.get('username')
-    get_roomid = request.GET.get('roomid')
-    print(get_userid)
-    print(get_roomid)
-    room = LiveRoom.objects.get(id=get_roomid)
-    user = User.objects.get(id=get_userid)
+    get_userid, get_roomid = request.GET.get('username'), request.GET.get('roomid')
+    room = LiveRoom.objects.get(pk=get_roomid)
+    user = User.objects.get(pk=get_userid)
     if room.is_silence == True:
         return HttpResponse('allsilence')
     else:
         punishment = Punishment.objects.filter(
-            room=room, user=user, punishment='S')
+            room=room, user_id=get_userid, punishment='S')
         if len(punishment) > 0:
             return HttpResponse('banone')
         else:
@@ -32,7 +29,6 @@ def echo(request):
 def banpublic(request):
     print('--banpulic--')
     get_roomid = request.GET.get('roomid')
-    print(get_roomid)
     room = LiveRoom.objects.get(id=get_roomid)
     room.is_silence = True
     room.save()
@@ -51,8 +47,7 @@ def canpublic(request):
 
 def outone(request):
     print('--outone--')
-    get_userid = request.GET.get('username')
-    get_roomid = request.GET.get('roomid')
+    get_userid, get_roomid = request.GET.get('username'), request.GET.get('roomid')
     print(get_roomid)
     room = LiveRoom.objects.get(id=get_roomid)
     user = User.objects.get(id=get_userid)
@@ -63,8 +58,7 @@ def outone(request):
 
 def banone(request):
     print('--banone--')
-    get_userid = request.GET.get('username')
-    get_roomid = request.GET.get('roomid')
+    get_userid, get_roomid = request.GET.get('username'), request.GET.get('roomid')
     room = LiveRoom.objects.get(id=get_roomid)
     user = User.objects.get(id=get_userid)
     new_punishment = Punishment(room=room, user=user, punishment='S')
@@ -81,3 +75,14 @@ def canspeakone(request):
     punishment = Punishment.objects.get(room=room, user=user, punishment='S')
     punishment.delete()
     return HttpResponse('success')
+
+def checkPermission(request):
+    user_id, room_id, punishment = request.GET.get('user_id', None), request.GET.get('room_id', None), request.GET.get('punishment', None)# 'K' or 'S'
+    if(user_id is None):
+        return HttpResponse(content = CODE['4'], status = 400)
+    p = Punishment.objects.filter(user_id = user_id)
+    if(room_id is not None):
+        p = p.filter(room_id = room_id)
+    if(punishment is not None):
+        p = p.filter(punishment = punishment)
+    return HttpResponse(content = CODE['12'] , status = 401) if len(p) > 0 else HttpResponse(content = CODE['0'])
